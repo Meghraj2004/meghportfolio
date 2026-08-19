25ca96ac0ae95697ef055cbbf5ac138096739951
Megharaj170804 <megharajdandgavhal2004@gmail.com>
2026-08-19 22:04:42 +0530

    Update routs.ts

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,6 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage";
-import nodemailer from "nodemailer";
 import path from "path";
 
 export async function registerRoutes(app: Express): Promise<Server> {
